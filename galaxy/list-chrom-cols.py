75902d6c63bfc64e629deb1ea04e1143cf7ce8be
Pierrick Roger Mele <pierrick.roger@gmail.com>
2016-08-28 18:51:00 +0200

    Now returns list formatted for galaxy input.

--- galaxy/list-chrom-cols.py
+++ galaxy/list-chrom-cols.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
 
 import argparse
-import os
+import subprocess
+import re
 
 def get_chrom_cols(dbtype, dburl, dbtoken = None, dbfields = None, dbmsmodes = None):
-    command = "../r-msdb/search-mz -d " + dbtype + " --url '" + dburl + "'"
+    
+    # Construct command to run
+    command = ["../r-msdb/search-mz", "-d", dbtype, "--url", dburl]
     if dbtoken is not None:
-        command += " --db-token " + dbtoken
+        command.extend(["--db-token", dbtoken])
     if dbfields is not None:
-        command += " --db-fields '" + dbfields + "'"
+        command.extend(["--db-fields", dbfields])
     if dbmsmodes is not None:
-        command += " --db-ms-modes '" + dbmsmodes + "'"
-    command += " --list-cols"
-    print(command)
-    os.system(command)
+        command.extend(["--db-ms-modes", dbmsmodes])
+    command.append("--list-cols")
+    
+    # Run command and get output
+    cols_table = subprocess.check_output(' '.join(command), shell = True)
+    
+    # Parse each output lines and fill the list
+    cols = []
+    i = 0
+    for i, line in enumerate(cols_table.split("\n")[1:]):
+        m = re.search('^"(.*)"\s*"(.*)"$', line)
+        if m is not None:
+            value = m.group(1)
+            title = m.group(2)
+            cols.append( (title, value, i == 0) )
+    
+    return cols
 
 ########
 # MAIN #
@@ -31,4 +47,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     args_dict = vars(args)
     
-    get_chrom_cols(**args_dict)
+    print(get_chrom_cols(**args_dict))
