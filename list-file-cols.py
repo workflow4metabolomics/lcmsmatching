1c95b9722c3e7b4b1a9e597b683c1f592c86c8d4
Pierrick Roger Mele <pierrick.rogermele@icloud.com>
2017-03-22 11:39:29 +0000

    Now set input col names with select lists.

---
+++ list-file-cols.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import csv
+import re
+
+def get_file_cols(file, preferred):
+
+	cols = []
+
+	with open(file if isinstance(file, str) else file.get_file_name(), 'rb') as f:
+
+		# Read file header
+		reader = csv.reader(f, delimiter = "\t", quotechar='"')
+		header = reader.next()
+
+		# Determine default value
+		default = 0
+		partial_match = -1
+		for p in preferred:
+			for i, c in enumerate(header):
+				if c == p:
+					default = i # Perfect match !
+					break
+				if re.match(p, c):
+					partial_match = i # Keep this partial match in case we find no perfect match
+			if default != 0:
+				break
+		if default == 0 and partial_match > 0:
+			default = partial_match
+
+		# Build list of cols
+		for i, c in enumerate(header):
+                    cols.append( (c, c, i == default) )
+
+	return cols
